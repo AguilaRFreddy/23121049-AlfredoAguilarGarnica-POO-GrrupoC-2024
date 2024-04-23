fd7b48339e9521405379af8bbe24ddafb95513f5
AGUILArFreddy <yoaguilar78@gmail.com>
2024-04-23 07:40:17 -0600

    cambio2

--- Documents/poo/Ejercicio libreria/Usuarios/Asistente.java
+++ Documents/poo/Ejercicio libreria/Usuarios/Asistente.java
@@ -1,6 +1,10 @@
 package Usuarios;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import Libreria.Libreria;
 import Usuarios.utils.Rol;
 
 public class Asistente extends Usuario{
@@ -22,4 +26,46 @@ public class Asistente extends Usuario{
         return String.format("%s, Fecha de inicio: %s, Sueldo: %.2f, RFC: %s",
          super.toString(), this.fechaInicio, this.sueldo, this.rfc) + ", Fecha de inicio: " + this.fechaInicio;
     }
+    public static void registrarAsistente() {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> datosComun = Libreria.obtenerDatosComun(Rol.ASISTENTE);
+
+        String nombre = datosComun.get(0);
+        String apellido = datosComun.get(1);
+        String telefono = datosComun.get(2);
+        String nombreUsuario = datosComun.get(3);
+        String contraseña = datosComun.get(4);
+        System.out.print("RFC: ");
+        String rfc = scanner.nextLine();
+        System.out.print("Sueldo: ");
+        double sueldo = scanner.nextDouble();
+        
+
+        Asistente asistente = new Asistente(nombre, apellido, telefono, sueldo, rfc.toUpperCase(), nombreUsuario, contraseña);
+        
+        if(!Libreria.usuarios.containsKey(Rol.ASISTENTE)) {
+            Libreria.usuarios.put(Rol.ASISTENTE, new ArrayList<Usuario>());   //eso va a entrar cuando la llave no exista, o sea que el hash este vacio, si no lo va a ignorar (en la segunda vez)
+        }
+
+        Libreria.usuarios.get(Rol.ASISTENTE).add(asistente); //aqui lo agrega en un lista especifica de clientes
+        System.out.println("\nRegistro exitoso");
+    }
+
+
+    public static void mostrarAsistentes() {
+        if(!Libreria.usuarios.containsKey(Rol.ASISTENTE) || Libreria.usuarios.get(Rol.ASISTENTE).isEmpty()) {
+            System.out.println("\nNo hay asistentes registrados aún");
+        }
+        else {
+            System.out.println("\nAsistentes en la biblioteca");
+            int x = 1;
+            for(Usuario usuario : Libreria.usuarios.get(Rol.ASISTENTE)) {
+                Asistente asistente = (Asistente) usuario;         //parseo
+                System.out.println("\n---- Asistente " + x + " ----\n");
+                System.out.println(asistente.toString());
+                x++;
+            }
+        }
+    }
+
 }
